9ef6b2d96603d6b7dd6105ca431a6dd077f907fe
franciscopedrodevtest24 <francisco.pedro@devtest.co.ao>
2025-04-11 14:01:18 +0100

    Add @mastra/deployer-vercel dependency and integrate VercelDeployer in index.ts

--- src/mastra/index.ts
+++ src/mastra/index.ts
@@ -1,13 +1,19 @@
 
 import { Mastra } from '@mastra/core/mastra';
 import { createLogger } from '@mastra/core/logger';
+import { VercelDeployer } from '@mastra/deployer-vercel';
 
 import { weatherAgent } from './agents';
-
+const deployer = new VercelDeployer({
+  teamSlug: 'agentes-do-tempo',
+  projectName: 'agente-do-tempo_dev',
+  token: 'a5CVAO9GhPRzN72FT90RO7Yt',
+});
 export const mastra = new Mastra({
   agents: { weatherAgent },
   logger: createLogger({
     name: 'Mastra',
     level: 'info',
   }),
+  deployer
 });
